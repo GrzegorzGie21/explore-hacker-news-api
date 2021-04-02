900a7cd60fc4eb27c56a8faf03c729aa0a71013b
GrzegorzGie21 <galazka.grzesiek@gmail.com>
2021-04-02 06:51:42 +0200

    Added snapshot tests with Jest

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,81 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
+import SearchForm from '../Search';
+import Button from '../Button';
+import Objects from '../Objects';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App/>);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('App', () => {
+  it('renders App component', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+  });
+  
+  test('snapshots for App component', () => {
+    const component = renderer.create(<App/>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('SearchForm', () => {
+  const props = {
+    title: 'Search',
+    onChange: () => {},
+    handleInput: () => {},
+    handleSubmit: () => {},
+    children: 'Next'
+  }
+  
+  it('renders SearchForm component', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<SearchForm {...props}>Search</SearchForm>, div);
+  });
+  
+  test('snapshots for SearchForm component', () => {
+    const component = renderer.create(<SearchForm {...props}>Search</SearchForm>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Button', () => {
+  it('renders Button component', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button>More</Button>, div);
+  });
+  
+  test('snapshots for Button component', () => {
+    const component = renderer.create(<Button>Next</Button>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Objects', () => {
+  const props = {
+    items: [
+      {title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y'},
+      {title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z'},
+    ],
+    removeItem: () => {}
+  };
+  
+  it('renders Objects component', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Objects {...props}/>, div);
+  });
+  
+  test('snapshots for Object Component', () => {
+    const component = renderer.create(<Objects {...props}/>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
